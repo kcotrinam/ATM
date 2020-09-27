b8d8a08326d851fa2a5fcc5d3698217cca8650d0
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-27 18:00:25 -0500

    user interface added

---
+++ bin/main.rb
@@ -0,0 +1,11 @@
+puts 'Enter your account Number'
+account_number = gets.chomp
+puts 'Enter your password'
+account_password = gets.chomp
+puts 'Verifying...'
+sleep(2)
+puts 'What do you want to do?'
+puts '1.- Consult savings'
+puts '2.- Withdraw'
+puts '3.- Deposit'
+option = gets.chomp
