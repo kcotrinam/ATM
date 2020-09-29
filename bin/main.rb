833e0dffabddabd54d2727adf2a0d1a07bc33ffc
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-29 00:43:00 -0500

    main file modularized

--- bin/main.rb
+++ bin/main.rb
@@ -38,17 +38,7 @@ def request_user_information(user)
   end
 end
 
-request_user_information(user)
-print "\033[2J\033[H"
-puts 'Welcome...'
-sleep(2)
-user_option = atm_options
-case user_option
-when 1
-  print "\033[2J\033[H"
-  puts atm.display
-when 2
-  print "\033[2J\033[H"
+def deposit_validator(atm)
   puts 'How much do you want to deposit?'
   amount = gets.chomp
   until atm.valid_money?(amount) && atm.valid_deposit?(amount)
@@ -56,17 +46,55 @@ when 2
     amount = gets.chomp
   end
   atm.deposit(amount)
-  puts "Your new savings is #{atm.display}"
-when 3
-  print "\033[2J\033[H"
-  puts 'How much do you awnt to withdraw'
+end
+
+def withdraw_validator(atm)
+  puts 'How much do you want to withdraw'
   amount = gets.chomp
   until atm.valid_money?(amount) && atm.valid_withdraw?(amount)
     puts "You don't have #{amount} saved. Try again"
     amount = gets.chomp
   end
   atm.withdraw(amount)
-  puts "Your new savings is #{atm.display}"
-else
-  puts "I'm option #{user_option}"
 end
+
+def atm_case(atm)
+  user_option = atm_options
+  case user_option
+  when 1
+    print "\033[2J\033[H"
+    puts atm.display
+  when 2
+    print "\033[2J\033[H"
+    deposit_validator(atm)
+    puts "Your new savings is #{atm.display}"
+  when 3
+    print "\033[2J\033[H"
+    withdraw_validator(atm)
+    puts "Your new savings is #{atm.display}"
+  else
+    puts "I'm option #{user_option}"
+  end
+end
+
+def run_atm(atm, user)
+  request_user_information(user)
+  print "\033[2J\033[H"
+  puts 'Welcome...'
+  sleep(2)
+  atm_case(atm)
+  continue?(atm, user)
+end
+
+def continue?(atm, user)
+  puts 'do you want to continue (Y/N)'
+  ans = gets.chomp.downcase
+  if ans == 'y'
+    print "\033[2J\033[H"
+    run_atm(atm, user)
+  else
+    puts 'Thanks for using our service'
+  end
+end
+
+run_atm(atm, user)
